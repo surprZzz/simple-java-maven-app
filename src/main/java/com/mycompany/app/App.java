2bc30261febc7696c9d40165bc319573a7329a96
surprZzz <1054259565@qq.com>
2023-06-09 20:48:44 +0800

    推送

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        System.out.println("推送分支");
     }
 
     public String getMessage() {
